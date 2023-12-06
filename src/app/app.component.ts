dce5d05c81e91d49988b3cd0ea0247e9af2bbd35
Pasindu Dinal <pasindu.dinal@randomsoftware.net>
2023-12-06 19:57:42 +0530

    Feat: Basic styling finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
-import {RecipesComponent} from "./recipes/recipes.component";
+import {BookComponent} from "./books/book.component";
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, RecipesComponent, ShoppingListComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, BookComponent, ShoppingListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Ingredient } from '../shared/ingredient.model';
+import { Book } from '../shared/book.model';
 import {ShoppingEditComponent} from "./shopping-edit/shopping-edit.component";
 import {CommonModule} from "@angular/common";
 
@@ -15,9 +15,9 @@ import {CommonModule} from "@angular/common";
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
+  books: Book[] = [
+    new Book('Apples', 5),
+    new Book('Tomatoes', 10),
   ];
 
   constructor() { }
